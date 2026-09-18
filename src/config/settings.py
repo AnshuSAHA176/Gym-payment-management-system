a18f33248a9eaf3a93653969b97ec3f422b98858
Anshu Saha <anshusahaa62@gmail.com>
2026-09-18 16:46:47 +0530

    add filters

--- src/config/settings.py
+++ src/config/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
 
     "rest_framework",
     "rest_framework_simplejwt",
-
+     'django_filters',
     "accounts",
     "members",
     "payment",
@@ -231,4 +231,5 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
--- src/members/views.py
+++ src/members/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets
 from .models import Member
 from rest_framework.permissions import IsAdminUser
 from .serializer import MemberSerializer
+from rest_framework import filters
 
 
 
@@ -11,6 +12,8 @@ class MemeberViewSet(viewsets.ModelViewSet):
     """
     A simple ViewSet for viewing and editing accounts.
     """
-    queryset = Member.objects.all()
+    queryset = Member.objects.all().order_by('name')
     serializer_class = MemberSerializer
     permission_classes = [IsAdminUser]
+    filter_backends=[filters.SearchFilter]
+    search_fields = ['name', 'phone',"member_id"]
