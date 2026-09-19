be3978ed90f2b04bb2b1fdaddc087a606ebbb025
Anshu Saha <anshusahaa62@gmail.com>
2026-09-19 13:56:42 +0530

    fixed

--- src/config/settings.py
+++ src/config/settings.py
@@ -144,7 +144,10 @@ else:
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": os.environ.get(
+            "REDIS_URL",
+            "redis://127.0.0.1:6379",
+        ),
     }
 }
 
